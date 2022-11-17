f2cc00f8a96a8aca1b45f94f2f20f7aa0617921a
Emir Uzunbel <emiruzunbel@yandex.com>
2022-11-17 12:43:34 +0300

    AcademyPython

---
+++ degiskenlerVeVeriTipleriniAnlamak.py
@@ -0,0 +1,17 @@
+
+baslik = 'HABERİNİZ OLSUN' #string
+vade = 10 #integer
+faizOrani = 1.63 #float
+faizOrani1 = 1.32
+faizOrani2 = 1.82
+
+print(baslik)
+print(type(baslik))
+print(type(vade))
+print(type(faizOrani))
+
+mesaj = "Hosgeldin"
+musteriAdi = "Berk"
+musteriSoyadi="Tonguc"
+sonucMesaj = mesaj+" "+musteriAdi+" "+musteriSoyadi
+print(sonucMesaj)
